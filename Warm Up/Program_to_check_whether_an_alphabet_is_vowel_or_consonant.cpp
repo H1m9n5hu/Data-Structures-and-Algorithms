eafbe7e2cfc058bb8f673e780142fb600a45a837
Himanshu Singh <hs098605@gmail.com>
2020-12-03 21:15:39 +0530

    Create Program_to_check_whether_an_alphabet_is_vowel_or_consonant.cpp

---
+++ Warm Up/Program_to_check_whether_an_alphabet_is_vowel_or_consonant.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a;
+    cout<<"Enter a alphabet in small letter : ";
+    cin>>a;
+    switch(a)
+    {
+        case 'a':
+            cout<<a<<" is an Vowel";
+            break;
+        case 'e':
+            cout<<a<<" is an Vowel";
+            break;
+        case 'i':
+            cout<<a<<" is an Vowel";
+            break;
+        case 'o':
+            cout<<a<<" is an Vowel";
+            break;
+        case 'u':
+            cout<<a<<" is an Vowel";
+            break;
+        default:
+            cout<<a<<" is an Consonant";
+    }
+    return 0;
+}
