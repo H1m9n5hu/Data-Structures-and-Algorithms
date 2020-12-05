ee5e2079037032951f6ea335b984033676a3f9b9
Himanshu Singh <hs098605@gmail.com>
2020-12-05 15:54:05 +0530

    Program to print all natural numbers from 1 and n

---
+++ Warm Up/P07.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    cout<<"All Natural numbers from 1 to "<<n<<" are given below"<<endl;
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
