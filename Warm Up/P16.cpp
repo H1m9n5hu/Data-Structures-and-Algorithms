7d73371504b8da4df13179546145bd3dd5e5aee2
Himanshu Singh <hs098605@gmail.com>
2020-12-06 21:04:05 +0530

    Write a C program to count number of digits in a number

--- Warm Up/P16.cpp
+++ Warm Up/P16.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int digits(long long x)
+int digits(long long x);
 int main()
 {
     long long int n,i=0,x;
     cout<<"Enter a number : ";
     cin>>n;
     x=n;
-        while(x!=0)
-        {
-            x=x/10;
-            i++;
-            if(x==0)
-                cout<<"Number of digits are "<<i<<" in "<<n;
-        }
+    while(x!=0)
+    {
+        x=x/10;
+        i++;
+        if(x==0)
+            cout<<"Number of digits are "<<i<<" in a number "<<n;
+    }
     return 0;
 }
