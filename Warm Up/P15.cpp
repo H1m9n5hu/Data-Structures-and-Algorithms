d31adcecd6ce7c4a9b095b1863daeb943f32a640
Himanshu Singh <hs098605@gmail.com>
2020-12-06 17:03:30 +0530

    Program to find sum of all odd numbers between 1 to n.

---
+++ Warm Up/P15.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,sum=0,i;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    if(n>0)
+    {
+       for(i=1;i<=n;i++)
+          if(i%2==1)
+             sum+=i;
+       cout<<"Sum of all Even numbers from 1 t0 "<<n<<" is "<<sum;
+    }
+    else
+        cout<<"Sorry,you entered invalid input."<<endl<<"You can enter only Natural numbers.";
+    return 0;
+}
