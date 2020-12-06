c34322629174b7fa8c3e1540eb2bda846b93ab65
Himanshu Singh <hs098605@gmail.com>
2020-12-06 15:19:04 +0530

    Write a C program to print all even numbers between 1 to 100.

---
+++ Warm Up/P11.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=100)
+    {
+        if(i%2==0)
+           cout<<i<<" ";
+        i++;
+    }
+    return 0;
+}
