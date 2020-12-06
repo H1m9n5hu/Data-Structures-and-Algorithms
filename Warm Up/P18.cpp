4b2c9089d77449136ca1d7dbd7e6b6033af504c7
Himanshu Singh <hs098605@gmail.com>
2020-12-06 19:32:59 +0530

    Program to calculate sum of digits of a number.

--- Warm Up/P18.cpp
+++ Warm Up/P18.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int digits(long long x);
 int main()
 {
-    int n,i=0,x;
+    long long int n,i=0,x;
     cout<<"Enter a number : ";
     cin>>n;
     x=n;
     while(x!=0)
     {
-        i=x%10;
+        i=i+x%10;
         x=x/10;
     }
-    cout<<"Sum of first and last digit of "<<n<<" is "<<(n%10)+i<<".";
+    i>=0?cout<<"Sum of all digits of "<<n<<" is "<<i<<".":cout<<"Sum of all digits of "<<n<<" is "<<-i<<".";
     return 0;
 }
