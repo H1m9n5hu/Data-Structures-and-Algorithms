6034d8f2ba3a37b70b859103fbd319fc66aba3f3
Himanshu Singh <hs098605@gmail.com>
2020-12-06 18:53:22 +0530

    Program to find sum of first and last digit of a number.

---
+++ Warm Up/P18.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i=0,x;
+    cout<<"Enter a number : ";
+    cin>>n;
+    x=n;
+    while(x!=0)
+    {
+        i=x%10;
+        x=x/10;
+    }
+    cout<<"Sum of first and last digit of "<<n<<" is "<<(n%10)+i<<".";
+    return 0;
+}
