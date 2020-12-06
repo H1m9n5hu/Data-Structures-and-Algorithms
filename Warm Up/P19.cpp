72f94972cc38a63cdc5f7dc89023c3f8fd2ca659
Himanshu Singh <hs098605@gmail.com>
2020-12-06 19:53:59 +0530

    Program to calculate product of digits of a number.

---
+++ Warm Up/P19.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int digits(long long x);
+int main()
+{
+    long long int n,i=1,x;
+    cout<<"Enter a number : ";
+    cin>>n;
+    x=n;
+    while(x!=0)
+    {
+        i=i*(x%10);
+        x=x/10;
+    }
+    i>=0?cout<<"Product of all digits of "<<n<<" is "<<i<<".":cout<<"Sum of all digits of "<<n<<" is "<<-i<<".";
+    return 0;
+}
