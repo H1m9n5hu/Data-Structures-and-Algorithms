3565d888a3c61faf8f591faf8352db9329d8c137
Himanshu Singh <hs098605@gmail.com>
2020-12-06 17:22:21 +0530

    Program to print multiplication table of any number.

--- Warm Up/P15.cpp
+++ Warm Up/P15.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n,sum=0,i;
+    int n,i;
     cout<<"Enter the value of n : ";
     cin>>n;
     if(n>0)
     {
-       for(i=1;i<=n;i++)
-          if(i%2==1)
-             sum+=i;
-       cout<<"Sum of all Even numbers from 1 t0 "<<n<<" is "<<sum;
+        cout<<"Multiplication table of "<<n<<" is given below"<<endl;
+        for(i=1;i<=10;i++)
+           cout<<i*n<<endl;
     }
     else
         cout<<"Sorry,you entered invalid input."<<endl<<"You can enter only Natural numbers.";
