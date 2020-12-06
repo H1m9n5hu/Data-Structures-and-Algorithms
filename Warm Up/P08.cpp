307605d80f835d92bc05742b102e3d8e0bdb8375
Himanshu Singh <hs098605@gmail.com>
2020-12-06 22:51:26 +0530

    Program to check whether a number is Prime or Not

--- Warm Up/P08.cpp
+++ Warm Up/P08.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main()
 {
+    bool flag=0;
     int n,x;
     cout<<"Enter a number to check that a number is prime or not : ";
     cin>>n;
     if(n>0)
     {
-        for(x=2;x<n;x++)
+        for(x=2;x<sqrt(n);x++)
           if(n%x==0)
-            break;
+          {
+              flag=1;
+              break;
+          }
     }
-    else
-        cout<<"Sorry,You entered wrong input."<<endl<<"You can input only natural numbers.";
-    if(n>0)
-        x==n?cout<<n<<" is a prime number.":cout<<n<<" is not a prime number.";
+    else cout<<"Sorry,You entered wrong input."<<endl<<"You can input only natural numbers.";
+
+    flag?cout<<n<<" is not a prime number":cout<<n<<" is a prime number";
     return 0;
 }
