510eeba212de18a9a3243e8ab50da5882d4dea07
Himanshu Singh <hs098605@gmail.com>
2020-12-09 20:49:53 +0530

    Program to find all factors of a number by switch case

---
+++ Warm Up/P25-S.cpp
@@ -0,0 +1,29 @@
+//Program to find all factors of a number by switch case
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i=2;
+    int factor(int,int);
+    cout<<"Enter a number : ";
+    cin>>n;
+    if(n!=0)
+    {
+        cout<<"Factors of "<<n<<" are 1 ";
+        while(n!=1)
+        {
+            switch(n%i==0)
+            {
+                case 0:
+                   i++;
+                   break;
+                case 1:
+                   n=n/i;
+                   cout<<i<<" ";
+            }
+        }
+    }
+    else cout<<"You can't enter 0.";
+    return 0;
+}
