de0c1baf0223731becfb6e1d1771551d98726685
Himanshu Singh <hs098605@gmail.com>
2020-12-09 20:05:55 +0530

    Program to print all ASCII character with their values

---
+++ Warm Up/P24.cpp
@@ -0,0 +1,13 @@
+//Program to print all ASCII character with their values.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<=255;i++)
+    {
+        cout<<i<<" = "<<(char)i<<endl;
+    }
+    return 0;
+}
