cc2249996a0fc0ffde655d2711b95d57ef67866b
Himanshu Singh <hs098605@gmail.com>
2020-12-19 12:49:29 +0530

    Find Smallest and Greatest element in an Array

---
+++ GFG/GF04.cpp
@@ -0,0 +1,29 @@
+// Find Smallest and Greatest element in an Array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen ( "input.txt", "r", stdin );
+    freopen ( "output.txt", "w", stdout );
+    
+    int t;
+    cin >> t;
+    
+    while ( t-- )
+    {
+        int N;
+        cin >> N;
+        int a [ N ], i, k;
+        for ( i = 0; i < N; i++ )
+            cin >> a[ i ];
+
+        sort ( a, a + N );
+
+        cout << "Smallest element in this Array is " << a [ 0 ] << endl;
+        cout << "Greatest element in this Array is " << a [ N - 1 ];
+        cout << endl << endl;
+    }
+    
+    return 0;
+}
