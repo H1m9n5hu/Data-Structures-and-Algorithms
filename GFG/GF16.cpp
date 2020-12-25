cac143c197e672088ea6166b8e36774c874267a8
Himanshu Singh <hs098605@gmail.com>
2020-12-25 14:15:41 +0530

    Print all the duplicates in the input string

---
+++ GFG/GF16.cpp
@@ -0,0 +1,41 @@
+// Program to Print all the duplicates in the input string
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen ( "input.txt", "r", stdin );
+    freopen ( "output.txt", "w", stdout );
+
+    int t;
+    cin >> t;
+
+    while ( t-- )
+    {
+        string S, s;
+        cin >> S;
+        int N, i, j, k = 0;
+        N = S.size();
+
+        for ( i = 0; i < N; i++ ) 
+        {
+           for ( j = 0; j < N; j++ )
+               if ( S [ i ] == S [ j ] )
+                   k++;
+           if ( k > 1 )
+           {
+               if ( s.find ( S [ i ] ) != string :: npos )
+               {
+               }
+               else
+               {
+                   cout << S [ i ] << " -> " << k << endl;
+                   s = s + S [ i ];
+               }
+            }
+            k = 0;
+        }
+    }
+
+    return 0;
+} 
