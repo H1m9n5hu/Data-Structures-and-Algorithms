afabcd08ca983845ab25d923e14839d93a986341
Himanshu Singh <hs098605@gmail.com>
2021-07-12 12:05:04 +0530

    Add Two Numbers
    Shivam is the youngest programmer in the world, he is just 12 years old. Shivam is learning programming and today he is writing his first program.
    
    The task is very simple: given two integers A and B, write a program to add these two numbers and output it.

---
+++ CodeChef Practice Programs/C03.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen ( "input.txt", "r", stdin );
+    freopen ( "output.txt", "w", stdout );
+    
+    int t;
+    cin >> t;
+    
+    while ( t-- )
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    
+    return 0;
+}
