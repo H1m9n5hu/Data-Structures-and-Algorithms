dd86c12105a1ccd75bb7815f3e33928536ae000b
Lucifer <57283974+HluciferS@users.noreply.github.com>
2022-04-01 16:51:35 +0530

    The One Where It All Began
    In a New York City coffee house called Central Perk, we're introduced to six friends: chef Monica Geller, data controller Chandler Bing who lives across the hall from Monica, Chandler's roommate/actor Joey Tribbiani, Monica's brother Ross Geller, new ageist Phoebe Buffay, and "born to be a wife" Rachel Green.
    
    Chef wants to divide N cookies between the six friends such that:
    
    Each friend gets at least one cookie.
    No two friends get the same number of cookies.
    Find if it is possible for Chef to do so.

---
+++ CodeChef Practice Programs/CP47.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if(n <= 21) {
+            cout << "No" << endl;
+        }
+        else {
+            cout << "Yes" << endl;
+        }
+    }
+    return 0;
+}
