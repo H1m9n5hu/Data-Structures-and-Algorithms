0e9d34523e04877877e32826418b24ee167af2ee
Lucifer <57283974+HluciferS@users.noreply.github.com>
2022-04-02 20:28:53 +0530

    Important Pages on CodeChef
    MoEngage noticed that some users are not aware of the practice page on CodeChef, and some others are not aware of the rated contests on CodeChef. So, MoEngage wants to send an email to the users depending on which of the following groups they fall into:
    
    If the user has never submitted on the practice page then send an email with the text:
    
    https://www.codechef.com/practice
    
    If the user has submitted on the practice page, but never participated in a contest then send an email with the text:
    
    https://www.codechef.com/contests
    
    If the user has submitted on the practice page as well as participated in a contest then send an email with the text:
    
    https://discuss.codechef.com
    
    so that they may discuss with other members of the community.
    
    Help MoEngage by writing a program that takes as input two integers A and B where:
    
    A=1 if the user has submitted on the practice page and 0 otherwise.
    B=1 if the user has participated in a contest and 0 otherwise.
    Output the appropriate link to be displayed in the email.

---
+++ CodeCheif/CP56.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if(a == 0)
+        cout << "https://www.codechef.com/practice";
+    else if(b == 0)
+        cout << "https://www.codechef.com/contests";
+    else
+        cout << "https://discuss.codechef.com";
+    return 0;
+}
